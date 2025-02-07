262165a52ee16330c78acdef4a76df5cb65c8a7a
Joshua <hola@joshua.cl>
2025-02-07 14:44:28 -0300

    Update Tasks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -36,4 +36,20 @@ export class HomeComponent {
       tasks.filter((task, position) => position !== index)
     );
   }
+
+  // Actualizar el estado de una tarea especifica mediante la posicion del array
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      // Con la funcion map recorro el estado actual de las tareas que es un array de objetos (tasks)
+      return tasks.map((task, position) => {
+        // Si la posicion (position) del elemento (task) del array (tasks) es igual al index (proporcionado como argumento para identificar la tarea a actualizar) entonces puedo actualizar esa tarea en especifico
+        if (position === index) {
+          // Tomo todas las propiedades de la tarea a actualizar y solo le cambio el valor de la propiedad completed, si es true ahora sera false y si es false ahora sera true
+          return { ...task, completed: !task.completed };
+        }
+        // Cuando la tarea no coincidad con la posicion proporcionada entonces devuelvo la tarea sin cambios
+        return task;
+      });
+    });
+  }
 }
